8f463fa5afdd1d501cb5bda0fbe10ad4ace6e85f
Shah Sultan <sultan91234567@gmail.com>
2025-06-21 12:10:01 +0600

    file read write in node using synchronous way. we may also use async way that may using callback hell.

---
+++ Node Express & MongoDB/Section 1/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+
+console.log(textInput);
+
+const textOut = `Welcome to Jahangirnagar university : ${textInput} \n Created on ${Date.now()} `;
+
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("written sucessfully");
